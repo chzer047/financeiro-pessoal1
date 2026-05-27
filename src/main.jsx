2934fae8bedcc1f31de189e13132ad72c60bd306
chzer047 <carloshspatricio@gmail.com>
2026-05-27 10:55:58 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import App from "./App.jsx";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
